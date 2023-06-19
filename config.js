5de9a08326d8c532a6993e4d1729442cc315454d
socona12 <136244360+socona12@users.noreply.github.com>
2023-06-19 22:13:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ global.prems = []
 // EN ➜ Add the language code which GataBot will use
 //  es = Español      id = Bahasa Indonesia       ar = عرب
 //  en = English      pt = Português 
-global.lenguajeGB = es  //<-- Predeterminado en idioma Español 
+global.lenguajeGB = ar  //<-- Predeterminado en idioma Español 
 
 // ES ➜ Consigue Apikey en https://platform.openai.com/account/api-keys
 global.openai_key = 'sk-0'
